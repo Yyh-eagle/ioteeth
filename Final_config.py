0d6308bbb3bdaaf88cc6110fccb792fa176483cc
闫禹昊 <14899946+Yyh-eagle@user.noreply.gitee.com>
2024-10-16 20:59:59 +0800

    缺陷版本，无法删除

--- Final_config.py
+++ Final_config.py
@@ -3,12 +3,19 @@ class Path(object):
 #添加固有属性
 
 
+   
+    def cap_save_path(self,position,frame):
 
-    
+        return f"/home/yyh/ioteeth/save_path/position{position}/cap/frame{frame}.jpg"
+        #return f"D:\学校学习\寒假与大二下\大创项目\pythonProject\save_path\position{position}\cap\\frame{frame}.jpg"
+    def cap_path(self,position):
+        return  f"/home/yyh/ioteeth/save_path/position{position}/cap/"
+        #return f"D:\学校学习\寒假与大二下\大创项目\pythonProject\save_path\position{position}\cap\\"
     #最终结果的保存路径
     def result_save_path(self,position):
         return f"/home/yyh/ioteeth/save_path/position{position}/results"
-    
+   
+   
 
     def obs_frame_path(self,position,iter):
         return f'obs://ioteeth/position{position}/frame{iter}.jpg'
